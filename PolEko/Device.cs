e29972e557e4a9732aa3c6dee5b5c94b638b0947
schmatteo <michal.skrzyszowski@hotmail.com>
2023-03-06 21:23:38 +0100

    Add Measurement class to Device.cs

--- PolEko/Device.cs
+++ PolEko/Device.cs
@@ -1,13 +1,36 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace PolEko;
 
 public class Device
 {
   public IPAddress IpAddress { get; }
+  public DateTime LastMeasurement { get; private set; } = DateTime.Now;
+  public int RefreshRate { get; set; } = 5;
 
   public Device(IPAddress ipAddress)
   {
     IpAddress = ipAddress;
+  }
+
+  public Device(IPAddress ipAddress, int refreshRate)
+  {
+    IpAddress = ipAddress;
+    RefreshRate = refreshRate;
+  }
+}
+
+public class Measurement
+{
+  public float Temperature { get; }
+  public int Humidity { get; }
+  public DateTime TimeOfMeasurement { get; }
+
+  public Measurement(float temperature, int humidity, DateTime dateTime)
+  {
+    Temperature = temperature;
+    Humidity = humidity;
+    TimeOfMeasurement = dateTime;
   }
 }
