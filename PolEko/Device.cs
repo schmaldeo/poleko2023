32c145c7cf071115a1d92289428831e7c79a0aee
schmatteo <michal.skrzyszowski@hotmail.com>
2023-03-09 10:34:22 +0100

    Change access modifiers in Device base class

--- PolEko/Device.cs
+++ PolEko/Device.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
@@ -9,23 +8,24 @@ namespace PolEko;
 
 public abstract class Device
 {
-  private HttpClient? Client;
-
-  public Device(IPAddress ipAddress, int port, string? id = null, int refreshRate = 5)
+  protected Device(IPAddress ipAddress, int port, string? id = null, int refreshRate = 5)
   {
     IpAddress = ipAddress;
     Port = port;
     Id = id;
     RefreshRate = refreshRate;
+
+    DeviceUri = new Uri($"http://{ipAddress}:{port}/");
   }
 
-  public string? Id { get; protected init; }
-  public IPAddress IpAddress { get; protected init; }
-  public int Port { get; protected init; }
   public DateTime LastMeasurement { get; protected init; } = DateTime.Now;
   public int RefreshRate { get; set; }
-  public abstract string Type { get; }
-  public abstract string Description { get; }
+  protected IPAddress IpAddress { get; }
+  protected abstract string Type { get; }
+  protected abstract string Description { get; }
+  protected Uri DeviceUri { get; }
+  private string? Id { get; }
+  private int Port { get; }
 
   /// <summary>
   ///   Custom <c>ToString()</c> implementation
@@ -77,28 +77,22 @@ public class WeatherDevice : Device
   }
 
   // Properties
-  public override string Type => "Weather Device";
+  protected override string Type => "Weather Device";
 
-  public override string Description => "Device used to measure temperature and humidity";
+  protected override string Description => "Device used to measure temperature and humidity";
 
   // Methods
-  public async Task<WeatherMeasurement> GetMeasurement()
+  public async Task<WeatherMeasurement> GetMeasurement(HttpClient client)
   {
-    HttpClient client = new();
-    var url = new Uri($"http://{IpAddress}:4040/");
-    client.BaseAddress = url;
-    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-    var res = await client.GetAsync("");
+    var res = await client.GetAsync(DeviceUri);
     if (res.StatusCode != HttpStatusCode.OK)
       // TODO: might not wanna throw an exception here
-      throw new Exception("API call did not return HTTP 200");
+      throw new Exception("placeholder");
     var data = await res.Content.ReadFromJsonAsync<WeatherMeasurement>();
 
-    client.Dispose();
 
     if (data != null) return data;
-    throw new Exception("lolo");
+    throw new Exception("placeholder");
   }
 
   public class WeatherMeasurement : Measurement
