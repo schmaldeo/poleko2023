bd02a0b7e5599b182e888cc3925122a11919a75c
schmatteo <michal.skrzyszowski@hotmail.com>
2023-03-09 17:46:25 +0100

    Change startup event handler

--- PolEko/App.xaml.cs
+++ PolEko/App.xaml.cs
@@ -1,8 +1,22 @@
-﻿namespace PolEko;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace PolEko;
 
 /// <summary>
 ///   Interaction logic for App.xaml
 /// </summary>
 public partial class App
 {
+  private void App_Startup(object sender, StartupEventArgs e)
+  {
+    MainWindow mainWindow = new();
+    mainWindow.Closing += delegate
+    {
+      // handle saving settings on closing
+      MessageBox.Show("Quittin");
+    };
+    mainWindow.Show();
+  }
 }
