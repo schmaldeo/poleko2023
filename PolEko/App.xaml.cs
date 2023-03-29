433216e03faaad1837b530a25724757283e8a54e
schmatteo <michal.skrzyszowski@hotmail.com>
2023-03-29 21:27:41 +0200

    Reformat code

--- PolEko/App.xaml.cs
+++ PolEko/App.xaml.cs
@@ -23,7 +23,6 @@ public partial class App
     // There's no need to modify many files when you add a new Device type. On top of that, it's often correct that
     // the end user (or, in case of open source software, a person that potentially makes a fork and tries to add
     // their own features) might not understand what certain features do, therefore the choice to use Reflection here.
-    // TODO: measure performance with and without reflection
     var deviceTypes = FindDerivedTypes(typeof(Device));
     foreach (var t in deviceTypes)
     {
