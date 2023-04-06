676b1fa2fd4af2422f15ec04dae0c77c3487bb42
schmatteo <michal.skrzyszowski@hotmail.com>
2023-04-06 17:33:30 +0200

    Move controls that belonged to SmartProDeviceHistoryControl to SmartProDeviceControl for simplicity

--- PolEko/ui/SmartProDeviceControl.xaml.cs
+++ PolEko/ui/SmartProDeviceControl.xaml.cs
@@ -1,17 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
+using System.IO;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
+using CsvHelper;
+using Microsoft.Win32;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using PolEko.util;
 
 namespace PolEko.ui;
 
 // Will create own HttpClient if not provided, not recommended
-public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INotifyPropertyChanged
+public partial class SmartProDeviceControl : IDeviceControl<SmartProDevice>
 {
   public static readonly DependencyProperty DeviceProperty =
     DependencyProperty.Register(nameof(Device), typeof(SmartProDevice), typeof(SmartProDeviceControl));
@@ -19,12 +27,35 @@ public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INot
   public static readonly DependencyProperty HttpClientProperty =
     DependencyProperty.Register(nameof(HttpClient), typeof(HttpClient), typeof(SmartProDeviceControl));
 
+  private List<SmartProMeasurement> _measurements = new();
+  private PlotModel? _plotModel;
   private SmartProDevice? _device;
   private bool _disposed;
   private HttpClient? _httpClient;
   private byte _retryCounter;
   private Status _status;
   private Timer? _timer;
+  
+  public List<SmartProMeasurement> Measurements
+  {
+    get => _measurements;
+    private set
+    {
+      _measurements = value;
+      OnPropertyChanged();
+    }
+  }
+
+  private PlotModel PlotModel
+  {
+    get => _plotModel;
+    set
+    {
+      _plotModel = value;
+      // This is required by OxyPlot design: https://oxyplot.readthedocs.io/en/latest/common-tasks/refresh-plot.html
+      PlotView.Model = value;
+    }
+  }
   
   public event PropertyChangedEventHandler? PropertyChanged;
   
@@ -37,9 +68,14 @@ public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INot
     Loaded += delegate
     {
       _httpClient = HttpClient ?? new HttpClient();
-      _device = Device;
-      HistoryControl.Device = Device;
     };
+  }
+
+  public SmartProDeviceControl(SmartProDevice device, HttpClient httpClient)
+  {
+    Device = device;
+    HttpClient = httpClient;
+    InitializeComponent();
   }
 
   public SmartProDevice Device
@@ -72,7 +108,7 @@ public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INot
   public async ValueTask DisposeAsync()
   {
     if (_timer == null || _disposed) return;
-    await _device!.InsertMeasurementsAsync();
+    await Device.InsertMeasurementsAsync();
     _disposed = true;
     GC.SuppressFinalize(this);
     await _timer.DisposeAsync();
@@ -81,7 +117,7 @@ public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INot
   public async void Dispose()
   {
     if (_timer == null || _disposed) return;
-    await _device!.InsertMeasurementsAsync();
+    await Device.InsertMeasurementsAsync();
     _disposed = true;
     GC.SuppressFinalize(this);
     _timer.Dispose();
@@ -89,7 +125,7 @@ public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INot
 
   private async void TimerCallback(object? _)
   {
-    var measurement = await _device!.GetMeasurementAsync(_httpClient!);
+    var measurement = await Device.GetMeasurementAsync(_httpClient!);
 
     if (measurement.NetworkError)
     {
@@ -128,7 +164,7 @@ public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INot
     // If connection was restored, put the previous timer params back
     if (CurrentStatus == Status.Error)
     {
-      _timer?.Change(_device.RefreshRate * 1000, _device.RefreshRate * 1000);
+      _timer?.Change(Device.RefreshRate * 1000, Device.RefreshRate * 1000);
       _retryCounter = 0;
     }
 
@@ -138,7 +174,7 @@ public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INot
   private void FetchData_OnClick(object sender, RoutedEventArgs e)
   {
     if (CurrentStatus == Status.Fetching) return;
-    _timer = new Timer(TimerCallback, null, 0, _device!.RefreshRate * 1000);
+    _timer = new Timer(TimerCallback, null, 0, Device.RefreshRate * 1000);
     CurrentStatus = Status.Fetching;
   }
 
@@ -151,8 +187,52 @@ public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INot
 
   private void DeleteDevice_OnClick(object sender, RoutedEventArgs e)
   {
-    DeviceRemoved?.Invoke(this, new RemoveDeviceEventArgs(_device!));
+    DeviceRemoved?.Invoke(this, new RemoveDeviceEventArgs(Device));
     Dispose();
+  }
+  
+  private async void Button_Click(object sender, RoutedEventArgs e)
+  {
+    // TODO
+    if (StartingDatePicker.Value is null || EndingDatePicker.Value is null) return;
+    var measurements =
+      await Database.GetMeasurementsAsync<SmartProMeasurement>((DateTime)StartingDatePicker.Value,
+        (DateTime)EndingDatePicker.Value, Device);
+    Measurements = measurements;
+    var plotModel = new PlotModel();
+
+    // Add X and Y axes to the plot
+    plotModel.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom, Title = "Time" });
+    plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Temperature" });
+
+    // Create a new LineSeries object
+    var lineSeries = new LineSeries();
+
+    // Add data points to the LineSeries from the List<DeviceMeasurement> object
+    foreach (var measurement in measurements)
+    {
+      lineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(measurement.TimeStamp), Math.Round(
+        (float)measurement.Temperature / 100, 2)));
+    }
+
+    // Add the LineSeries to the PlotModel
+    plotModel.Series.Add(lineSeries);
+    PlotModel = plotModel;
+  }
+  
+  private async void CsvExport_Click(object sender, RoutedEventArgs e)
+  {
+    var dialog = new SaveFileDialog
+    {
+      FileName = "measurements",
+      DefaultExt = "csv",
+      Filter = "CSV (Comma delimited)|*.csv"
+    };
+    var result = dialog.ShowDialog();
+    if (result != true) return;
+    await using var writer = new StreamWriter(dialog.FileName);
+    await using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+    await csvWriter.WriteRecordsAsync(_measurements);
   }
 
   public enum Status
@@ -161,16 +241,16 @@ public partial class SmartProDeviceControl : IDisposable, IAsyncDisposable, INot
     Fetching,
     Error
   }
+}
 
-  public class RemoveDeviceEventArgs : EventArgs
+public class RemoveDeviceEventArgs : EventArgs
+{
+  public RemoveDeviceEventArgs(Device device)
   {
-    public RemoveDeviceEventArgs(Device device)
-    {
-      Device = device;
-    }
-
-    public Device Device { get; }
+    Device = device;
   }
+
+  public Device Device { get; }
 }
 
 [ValueConversion(typeof(int), typeof(string))]
