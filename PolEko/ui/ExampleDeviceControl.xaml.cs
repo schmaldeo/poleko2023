d1cb4081785248233ae5658ff739fc9753f80516
schmatteo <michal.skrzyszowski@hotmail.com>
2023-04-06 18:10:22 +0200

    Add ExampleDeviceControl

---
+++ PolEko/ui/ExampleDeviceControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PolEko.ui;
+
+public partial class ExampleDeviceControl : IDeviceControl<ExampleDevice>
+{
+  public ExampleDevice Device { get; }
+  public HttpClient HttpClient { get; }
+
+  public event PropertyChangedEventHandler? PropertyChanged;
+  public event EventHandler<DeviceRemovedEventArgs>? DeviceRemoved;
+  
+  public ExampleDeviceControl()
+  {
+    InitializeComponent();
+  }
+
+  public ExampleDeviceControl(ExampleDevice device, HttpClient client)
+  {
+    InitializeComponent();
+    Device = device;
+    HttpClient = client;
+  }
+  
+  private void DeleteDevice_OnClick(object sender, RoutedEventArgs e)
+  {
+    DeviceRemoved?.Invoke(this, new DeviceRemovedEventArgs(Device));
+    Dispose();
+  }
+
+  public void Dispose(){}
+  
+  public async ValueTask DisposeAsync() {}
+}
